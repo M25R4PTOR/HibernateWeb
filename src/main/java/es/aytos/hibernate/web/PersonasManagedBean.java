d2032c541bbf7e545d7e3c3eacffae94aa6342e1
M25R4PTOR <m25r4ptor@gmail.com>
2018-04-24 09:46:35 +0200

    Commit inicio

---
+++ src/main/java/es/aytos/hibernate/web/PersonasManagedBean.java
@@ -0,0 +1,30 @@
+package es.aytos.hibernate.web;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import es.aytos.hibernate.hibernate_dual.modelo.Persona;
+import es.aytos.hibernate.hibernate_dual.repositorio.RepositorioPersona;
+
+@ManagedBean
+@RequestScoped
+public class PersonasManagedBean implements Serializable{
+
+	private List<Persona> personas;
+	
+	public PersonasManagedBean() {
+	}
+	
+	@PostConstruct
+	public void inicializar() {
+		this.personas = RepositorioPersona.consultar("", "", "", null);
+	}
+
+	public List<Persona> getPersonas() {
+		return personas;
+	}
+}
